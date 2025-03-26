9756d3abe35431eb74e253cff234ed8531d72157
sdz <15045353+hhusdz9396@user.noreply.gitee.com>
2025-03-27 00:55:44 +0800

    暂时完成前后端交互

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
 from flask import Flask, json, request, jsonify
 from flask_cors import CORS
 import pymysql
+import cv2
+import numpy as np
+import os
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resource={r'/*': {'origins': '*'}})
 
-# #添加用户节点
-@app.route('/adduser', methods=['get', ])
+@app.route('/adduser', methods=['get', 'post'])
 def adduser():
-    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='root', charset='utf8')
-    cursor = conn.cursor()
-    conn.commit()
-    cursor.execute('use gpaper')
-    datalist = []
-    datalist.append(request.form.get("account"))
-    datalist.append(request.form.get("password"))
-    print(datalist)
-    # 进入数据库bdjs
-    # 向newscontent添加数据
-    sql = "SELECT * FROM `userinfo` WHERE `account`= '" + str(datalist[0]) + "';"
-    res1 = cursor.execute(sql)
-    if res1 == 1:
-        return "no"
-    sql2 = "INSERT INTO `userinfo`(`account`, `password`) VALUES('" + str(datalist[0]) + "','" + str(
-        datalist[1]) + "');"
-    res = cursor.execute(sql2)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return "ok"
+    username = request.form.get("username")
+    password = request.form.get("password")
+    print(username)
+    print(password)
+    return "已接收用户信息"
+
+@app.route('/uploadbefore', methods=['get', 'post'])
+def uploadbefore():
+    username = request.form.get("username")
+    img = request.files['file']
+    picname=img.filename
+    file = img.read()
+
+    file = cv2.imdecode(np.frombuffer(file, np.uint8), cv2.IMREAD_COLOR)  # 解码为ndarray
+    imgfile1_path = "./static/images/"+username+"/B/"
+    if not os.path.exists(imgfile1_path):
+        os.makedirs(imgfile1_path)
+    img1_path = os.path.join(imgfile1_path, picname)
+    cv2.imwrite(filename=img1_path, img=file)
+    url = "http://127.0.0.1:5000/static/images/"+username+"/B/" + picname
+    print(url)
+
+    tempmap = {"url": url}
+    return jsonify(tempmap)
+
+
+@app.route('/uploadafter', methods=['get', 'post'])
+def uploadafter():
+    username = request.form.get("username")
+    img = request.files['file']
+    picname=img.filename
+    file = img.read()
+
+    file = cv2.imdecode(np.frombuffer(file, np.uint8), cv2.IMREAD_COLOR)  # 解码为ndarray
+    imgfile1_path = "./static/images/"+username+"/A/"
+    if not os.path.exists(imgfile1_path):
+        os.makedirs(imgfile1_path)
+    img1_path = os.path.join(imgfile1_path, picname)
+    cv2.imwrite(filename=img1_path, img=file)
+    url = "http://127.0.0.1:5000/static/images/"+username+"/A/" + picname
+    print(url)
+
+    tempmap = {"url": url}
+    return jsonify(tempmap)
+
 
 
 if __name__ == '__main__':
